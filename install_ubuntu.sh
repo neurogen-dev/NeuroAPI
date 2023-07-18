b2efde6ec9d5564c58e4177ce0678876ef9577a2
Roman S <next.attack@gmail.com>
2023-07-19 00:19:03 +0800

    Добавлен скрипт запуска для linux

---
+++ install_ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo apt install python3-full git
+
+python3 --version
+git --version
---
+++ start_ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git checkout main
+git fetch --all
+git reset --hard origin/main
+git pull
+
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install -U setuptools
+python3 -m pip install -r requirements.txt
+
+echo Opening NeurogenGPT...
+
+export HIDE_LOCAL_MODELS=true
+
+source venv/bin/activate
+
+python3 webui.py
