bd868f1c91c09afb5182fd06f8e34eb291fba285
Roman S <next.attack@gmail.com>
2023-07-19 00:21:28 +0800

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,3 +4,9 @@ sudo apt install python3-full git
 
 python3 --version
 git --version
+
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install -U setuptools
+python3 -m pip install -r requirements.txt
