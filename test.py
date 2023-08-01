a977c5103253cfdf95da264394aeb7d145f2d2d9
Roman S <next.attack@gmail.com>
2023-08-01 08:37:03 +0800

    Обновление одного из провайдеров

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import g4f
 from fp.fp import FreeProxy
 
-proxy = FreeProxy(country_id=['US', 'GB', 'FL'], timeout=0.5, rand=True, https=True).get()
+proxy = FreeProxy(country_id=['US'], timeout=0.5, rand=True).get()
 # Set with provider
 stream = False
 print(proxy)
-response = g4f.ChatCompletion.create(model='claude-2', provider=g4f.Provider.ClaudeAI, messages=[
-                                     {"role": "user", "content": "hello"}], stream=False)
+response = g4f.ChatCompletion.create(model='text-davinci-003', provider=g4f.Provider.Vercel, messages=[
+                                     {"role": "user", "content": "hello"}], stream=False, proxy=proxy)
 
 if stream:
     for message in response:
