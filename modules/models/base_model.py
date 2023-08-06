f33ea30146bdc482958c20a74aa71eaf8fcc0eea
Roman S <next.attack@gmail.com>
2023-08-06 20:18:32 +0800

    Исправлено некорректное отображение загруженной истории

--- modules/models/base_model.py
+++ modules/models/base_model.py
@@ -606,7 +606,7 @@ class BaseLLMModel:
                 history_file_path = os.path.join(HISTORY_DIR, user_name, filename)
             else:
                 history_file_path = filename
-            with open(history_file_path, "r") as f:
+            with open(history_file_path, "r", encoding='utf8') as f:
                 json_s = json.load(f)
             try:
                 if type(json_s["history"][0]) == str:
