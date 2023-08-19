3b85fb907f89129238bd623f5872c7df3f3ccba2
Roman S <next.attack@gmail.com>
2023-08-20 01:33:06 +0800

    Автоматическая проверка git и python

--- start_portable_webui.bat
+++ start_portable_webui.bat
@@ -1,15 +1,4 @@
 @echo off
-set pypath=home = %~dp0python
-set venvpath=_ENV=%~dp0venv
-if exist venv (powershell -command "$text = (gc venv\pyvenv.cfg) -replace 'home = .*', $env:pypath; $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False);[System.IO.File]::WriteAllLines('venv\pyvenv.cfg', $text, $Utf8NoBomEncoding);$text = (gc venv\scripts\activate.bat) -replace '_ENV=.*', $env:venvpath; $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False);[System.IO.File]::WriteAllLines('venv\scripts\activate.bat', $text, $Utf8NoBomEncoding);")
-
-for /d %%i in (tmp\tmp*,tmp\pip*) do rd /s /q "%%i" 2>nul || ("%%i" && exit /b 1) & del /q tmp\tmp* > nul 2>&1 & rd /s /q pip\cache 2>nul
-
-set appdata=tmp
-set userprofile=tmp
-set temp=tmp
-set PATH=git\cmd;python;venv\scripts
-
 echo Opening NeuroGPT...
 
 set HIDE_LOCAL_MODELS=true 
@@ -37,6 +26,18 @@ if not exist git (
   tar -xf git.zip
   del git.zip /q
 )
+
+set pypath=home = %~dp0python
+set venvpath=_ENV=%~dp0venv
+if exist venv (powershell -command "$text = (gc venv\pyvenv.cfg) -replace 'home = .*', $env:pypath; $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False);[System.IO.File]::WriteAllLines('venv\pyvenv.cfg', $text, $Utf8NoBomEncoding);$text = (gc venv\scripts\activate.bat) -replace '_ENV=.*', $env:venvpath; $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False);[System.IO.File]::WriteAllLines('venv\scripts\activate.bat', $text, $Utf8NoBomEncoding);")
+
+for /d %%i in (tmp\tmp*,tmp\pip*) do rd /s /q "%%i" 2>nul || ("%%i" && exit /b 1) & del /q tmp\tmp* > nul 2>&1 & rd /s /q pip\cache 2>nul
+
+set appdata=tmp
+set userprofile=tmp
+set temp=tmp
+set PATH=git\cmd;python;venv\scripts
+
 
 echo Checking for updates...
 
