b25198f1af7333327f28fc17383e51f24edf2ca8
Roman S <next.attack@gmail.com>
2023-08-26 02:04:53 +0800

    Update start_portable_webui.bat

--- start_portable_webui.bat
+++ start_portable_webui.bat
@@ -57,8 +57,8 @@ python -m venv venv
 call venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -U setuptools 
-python -m pip install -r requirements.txt
 python -m pip install whl\quickjs-1.19.2-cp311-cp311-win_amd64.whl
+python -m pip install -r requirements.txt
 python -m pip install -r requirements_advanced.txt
 
 echo Completed.
