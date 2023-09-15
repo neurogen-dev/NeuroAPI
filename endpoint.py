ea82609b18535a4356b40d4ccc7605967bc12f2a
Roman S <next.attack@gmail.com>
2023-09-16 01:20:31 +0800

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,4 +1,5 @@
 import fastwsgi
+import socket
 from backend import app
 from multiprocessing import Process
 
@@ -7,5 +8,15 @@ def run_api_server():
 
 
 if __name__ == "__main__":
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    site_config = {
+        'host': '0.0.0.0',
+        'port': 1337,
+        'debug': False
+         }
+    print(f"Running on http://127.0.0.1:{site_config['port']}")
+    print(f"Running on http://{ip_address}:{site_config['port']}")
+
     api_process = Process(target=run_api_server) 
     api_process.start()
