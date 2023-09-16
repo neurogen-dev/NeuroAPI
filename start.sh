0141e66019462e495cc0e544f8f23c5e1ba4cc4f
Евгений Долгов <dolgov-ev@ranepa.ru>
2023-09-16 21:57:48 +0300

    update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/bash
-
-
 export HIDE_LOCAL_MODELS=true
-
-cp  config.json config_temp.json
-git checkout main
 git fetch --all
-git reset --hard origin/main
+git switch main
 git pull
-REM Восстанавливаем оригинальный файл config.json
-cp  config_temp.json config.json
-rm config_temp.json
-
-python -m venv venv
-source venv\Scripts\activate.bat
-python -m pip install --upgrade pip
-python -m pip install -U setuptools
-python -m pip install -r requirements.txt
-python -m spacy download en_core_web_sm
-python -m spacy download zh_core_web_sm
-python -m spacy download ru_core_news_sm
-
-
+source venv/bin/activate
 python webui_ru.py
+
 
