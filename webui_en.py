e82e4d41d7cc3c484ba602bbe95187dddd82013b
Daniil <139611852+NealBelov@users.noreply.github.com>
2023-09-18 05:51:18 +0300

    upd webui

--- webui_en.py
+++ webui_en.py
@@ -503,7 +503,7 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
 
 logging.info(
     colorama.Back.BLUE
-    + "Version" + VERSION
+    + "" + VERSION
     + colorama.Style.RESET_ALL
 )
 
