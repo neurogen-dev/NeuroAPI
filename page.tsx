ecef3d7de1f380bac48ba1be73a240e10983c8bf
Roman S <70670181+Em1tSan@users.noreply.github.com>
2023-10-22 03:13:13 +0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,17 @@
+import { Analytics } from "@vercel/analytics/react";
+
+import { Home } from "./components/home";
+
+import { getServerSideConfig } from "./config/server";
+
+const serverConfig = getServerSideConfig();
+
+export default async function App() {
+  return (
+    <>
+      <Home />
+      {serverConfig?.isVercel}
+      {serverConfig?.isVercelWebAnalytics && <Analytics />}
+    </>
+  );
+}
