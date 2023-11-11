ba30772c8bc5cf5549860869d0ba22d4d26f656d
Roman S <next.attack@gmail.com>
2023-11-12 02:02:07 +0800

    Fix g4f

--- start_endpoint.bat
+++ start_endpoint.bat
@@ -1,17 +1,6 @@
 @echo off
 echo Opening NeuroGPT endpoint...
 
-echo Checking for updates...
-REM Создаем временную копию файла config.json
-copy /Y config.json config_temp.json
-git checkout main
-git fetch --all
-git reset --hard origin/main
-git pull
-REM Восстанавливаем оригинальный файл config.json
-copy /Y config_temp.json config.json
-del config_temp.json
-
 python -m venv venv
 call venv\Scripts\activate.bat
 python -m pip install --upgrade pip
@@ -21,5 +10,5 @@ python -m pip install -U g4f
 echo Completed.
 echo Running NeuroGPT...
 
-python -m g4f.api
+g4f api
 pause
